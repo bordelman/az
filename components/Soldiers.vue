a9129149d008a04faf476221b108be53ead5679e
Jakub Peca <jakub.peca@orgis.cz>
2025-02-22 23:57:29 +0100

    table

--- components/Soldiers.vue
+++ components/Soldiers.vue
@@ -1,110 +1,18 @@
 <template>
-  <n-table
-    :single-line="false"
+  <NDataTable
     striped
-  >
-    <thead>
-      <tr>
-        <th
-          v-if="logged.rank.id > 7"
-          :class="[direction, { active: sortBy === 'personalNumber' }]"
-          @click="changeOrder('personalNumber')"
-        >
-          Osobní číslo
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'rank' }]"
-          @click="changeOrder('rank')"
-        >
-          Hodnost
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'firstname' }]"
-          @click="changeOrder('firstname')"
-        >
-          Jméno
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'lastname' }]"
-          @click="changeOrder('lastname')"
-        >
-          Příjmení
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'position' }]"
-          @click="changeOrder('position')"
-        >
-          Pozice
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'company' }]"
-          @click="changeOrder('company')"
-        >
-          Rota
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'platoon' }]"
-          @click="changeOrder('platoon')"
-        >
-          Četa
-        </th>
-        <th
-          :class="[direction, { active: sortBy === 'squad' }]"
-          @click="changeOrder('squad')"
-        >
-          Družstvo
-        </th>
-        <th
-          v-if="logged.rank.id > 7"
-          :class="[direction, { active: sortBy === 'medicalExaminationDue' }]"
-          @click="changeOrder('medicalExaminationDue')"
-        >
-          Lékařská prohlídka
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <template
-        v-for="(soldier, index) of soldiers"
-        :key="'soldier-' + index"
-      >
-        <tr>
-          <td v-if="logged.rank.id > 7">
-            <NuxtLink
-              class="row"
-              :to="
-                !clickAction ? '/soldiers/' + soldier.personalNumber : undefined
-              "
-            >
-              {{ soldier.personalNumber }}
-            </NuxtLink>
-          </td>
-          <td>{{ soldier.rank.abbreviation }}.</td>
-          <td>{{ soldier.firstname }}</td>
-          <td>{{ soldier.lastname }}</td>
-          <td>{{ soldier.position.position }}</td>
-          <td>{{ soldier.company }}</td>
-          <td>{{ soldier.platoon }}</td>
-          <td>{{ soldier.squad }}</td>
-          <td v-if="logged.rank.id > 7">
-            {{
-              new Date(soldier.medicalExaminationDue).toLocaleDateString("cs")
-            }}
-          </td>
-        </tr>
-      </template>
-    </tbody>
-    <div class="body"></div>
-  </n-table>
+    :columns="columns"
+    :data="soldiers"
+    :row-key="(row: RowData) => row.personalNumber"
+  />
 </template>
 
 <script setup lang="ts">
-import { NTable } from "naive-ui";
+import { NDataTable, NTable } from "naive-ui";
 import { getSoldiers } from "@/utils/api";
 import type { ISoldier } from "~/types";
 
-const { clickAction, medicalExaminationDue } = defineProps({
-    clickAction: Function,
+const { medicalExaminationDue } = defineProps({
     medicalExaminationDue: {
       type: String,
       default: "1970-01-01",
@@ -113,7 +21,201 @@ const { clickAction, medicalExaminationDue } = defineProps({
   logged = useState<ISoldier>("logged"),
   sortBy = ref("personalNumber"),
   direction = ref("asc"),
-  soldiers: Ref<Array<ISoldier>> = ref([]);
+  soldiers: Ref<Array<ISoldier>> = ref([]),
+  columns = computed(() => {
+    const squadFiltrOptions = soldiers.value
+        .reduce((acc: Array<ISoldier>, currentValue: ISoldier) => {
+          if (!acc.some((soldier) => soldier.squad === currentValue.squad))
+            acc.push(currentValue);
+          return acc;
+        }, [])
+        .map((item) => {
+          return {
+            label: item.squad,
+            value: item.squad,
+          };
+        })
+        .sort((item1, item2) => item1.value - item2.value),
+      platoonFiltrOptions = soldiers.value
+        .reduce((acc: Array<ISoldier>, currentValue: ISoldier) => {
+          if (!acc.some((soldier) => soldier.platoon === currentValue.platoon))
+            acc.push(currentValue);
+          return acc;
+        }, [])
+        .map((item) => {
+          return {
+            label: item.platoon,
+            value: item.platoon,
+          };
+        })
+        .sort((item1, item2) => item1.value - item2.value),
+      companyFiltrOptions = soldiers.value
+        .reduce((acc: Array<ISoldier>, currentValue: ISoldier) => {
+          if (!acc.some((soldier) => soldier.company === currentValue.company))
+            acc.push(currentValue);
+          return acc;
+        }, [])
+        .map((item) => {
+          return {
+            label: item.company,
+            value: item.company,
+          };
+        })
+        .sort((item1, item2) => item1.value - item2.value),
+      positionFiltrOptions = soldiers.value
+        .reduce((acc: Array<ISoldier>, currentValue: ISoldier) => {
+          if (
+            !acc.some(
+              (soldier) => soldier.position.id === currentValue.position.id
+            )
+          )
+            acc.push(currentValue);
+          return acc;
+        }, [])
+        .map((item) => {
+          return {
+            label: item.position.position,
+            value: item.position.id,
+          };
+        })
+        .sort((item1, item2) => item1.value - item2.value),
+      rankFiltrOptions = soldiers.value
+        .reduce((acc: Array<ISoldier>, currentValue: ISoldier) => {
+          if (!acc.some((soldier) => soldier.rank.id === currentValue.rank.id))
+            acc.push(currentValue);
+          return acc;
+        }, [])
+        .map((item) => {
+          return {
+            label: item.rank.rank,
+            value: item.rank.id,
+          };
+        })
+        .sort((item1, item2) => item1.value - item2.value),
+      columns = [
+        {
+          title: "Osobní číslo",
+          key: "personalNumber",
+          sorter: (soldier1: ISoldier, soldier2: ISoldier) =>
+            soldier1.personalNumber - soldier2.personalNumber,
+          defaultSortOrder: "ascend",
+        },
+        {
+          title: "Hodnost",
+          key: "rank",
+          render(soldier: ISoldier) {
+            return h("span", soldier.rank.abbreviation + ".");
+          },
+          sorter: (soldier1: ISoldier, soldier2: ISoldier) =>
+            soldier1.rank.id - soldier2.rank.id,
+          filterOptions: rankFiltrOptions,
+          filter(rank: number, soldier: ISoldier) {
+            return soldier.rank.id === rank;
+          },
+        },
+        {
+          title: "Jméno",
+          key: "firstname",
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              soldier1.firstname.localeCompare(soldier2.firstname),
+            multiple: 5,
+          },
+        },
+        {
+          title: "Příjmení",
+          key: "lastname",
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              soldier1.lastname.localeCompare(soldier2.lastname),
+            multiple: 6,
+          },
+        },
+        {
+          title: "Pozice",
+          key: "position",
+          render(soldier: ISoldier) {
+            return h("span", soldier.position.position);
+          },
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              soldier1.position.position.localeCompare(
+                soldier2.position.position
+              ),
+            multiple: 7,
+          },
+          filterOptions: positionFiltrOptions,
+          filter(position: number, soldier: ISoldier) {
+            return soldier.position.id === position;
+          },
+        },
+        {
+          title: "Rota",
+          key: "company",
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              soldier1.company - soldier2.company,
+            multiple: 10,
+          },
+          filterOptions: companyFiltrOptions,
+          filter(company: number, soldier: ISoldier) {
+            return soldier.company === company;
+          },
+        },
+        {
+          title: "Četa",
+          key: "platoon",
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              (soldier1.platoon || 0) - (soldier2.platoon || 0),
+            multiple: 9,
+          },
+          filterOptions: platoonFiltrOptions,
+          filter(platoon: number, soldier: ISoldier) {
+            return soldier.platoon === platoon;
+          },
+        },
+        {
+          title: "Družstvo",
+          key: "squad",
+          sorter: {
+            compare: (soldier1: ISoldier, soldier2: ISoldier) =>
+              (soldier1.squad || 0) - (soldier2.squad || 0),
+            multiple: 8,
+          },
+          filterOptions: squadFiltrOptions,
+          filter(squad: number, soldier: ISoldier) {
+            return soldier.squad === squad;
+          },
+        },
+        {
+          title: "Lékařská prohlídka",
+          key: "medicalExaminationDue",
+          filterOptions: [
+            {
+              label: "K dnešnímu datu",
+              value: Date.now(),
+            },
+          ],
+          filter(value: number, soldier: ISoldier) {
+            return new Date(soldier.medicalExaminationDue).getTime() >= value;
+          },
+          render(soldier: ISoldier) {
+            return h(
+              "span",
+              new Date(soldier.medicalExaminationDue).toLocaleDateString("cs")
+            );
+          },
+        },
+      ];
+
+    if (logged.value.rank.id < 7) {
+      columns.pop();
+      columns.shift();
+    }
+
+    return columns;
+  });
 
 function changeOrder(param) {
   if (param === sortBy.value) {
@@ -121,12 +223,6 @@ function changeOrder(param) {
   } else {
     sortBy.value = param;
     direction.value = "asc";
-  }
-}
-
-function action(personalNumber: number) {
-  if (clickAction) {
-    clickAction(personalNumber);
   }
 }
 
