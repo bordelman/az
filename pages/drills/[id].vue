2dd4cc3072a694b69300e5182732a62d42fb1d7d
Jakub Peca <jakub.peca@orgis.cz>
2025-02-22 23:12:08 +0100

    nomination edit

--- pages/drills/[id].vue
+++ pages/drills/[id].vue
@@ -95,6 +95,22 @@
               </template>
               Zažádal o poskytnutí ubytování
             </NTooltip>
+            <NTooltip v-if="nomination.soldier.personalNumber == myId">
+              <template #trigger>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  height="16px"
+                  width="16px"
+                  @click="editNomination"
+                >
+                  <path
+                    d="m487.4 315.7-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3l-42.6 24.6c-17.9-15.4-38.5-27.3-60.8-35.1v-49.1c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7v49.2c-22.2 7.9-42.8 19.8-60.8 35.1l-42.5-24.6c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14l42.6 24.6c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zm-231.4 20.3c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"
+                  />
+                </svg>
+              </template>
+              Upravit parkování/ubytování
+            </NTooltip>
           </li>
         </ul>
       </section>
@@ -189,6 +205,7 @@ const { isLoading } = useLayout(),
   parking: Ref<IParking> = ref({ color: "", spz: "", brand: "" }),
   drill = ref((await getDrills({ id }))[0]),
   nominations = ref(await getDrillNominations(id)),
+  myNomination = computed(() => nominations.value.find(nomination => nomination.soldier.personalNumber === myId)),
   present = computed(() =>
     nominations.value.filter(
       (nomination) => nomination.status === EAttendance.Present
@@ -237,6 +254,14 @@ async function callRemoveDrill() {
   await removeDrill(drill.value.id);
 
   useRouter().replace("/drills");
+}
+
+async function editNomination() {
+  if (myNomination.value?.parking) {
+    parking.value = myNomination.value?.parking as IParking;
+  }
+  accommodation.value = !!myNomination.value?.accommodation;
+  showParkingModal.value = true;
 }
 
 async function addMe() {
@@ -289,6 +314,15 @@ async function removeMe() {
 
       .soldier {
         display: flex;
+        align-items: center;
+        gap: 5px;
+
+        svg {
+          cursor: pointer;
+          path {
+            fill: currentColor;
+          }
+        }
 
         .badge {
           cursor: pointer;
